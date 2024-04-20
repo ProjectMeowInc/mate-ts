2613324d4e41e06e6aab80a45e06fc64e3b0bdc2
lx1cx <lagunov.2019@list.ru>
2024-04-20 13:43:30 +0500

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import dts from "vite-plugin-dts"
+import path from "path"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "./src/index.ts"),
+            name: "mate",
+            formats: ["umd"],
+            fileName: (format) => `mate-ts.${format}.js`,
+        },
+    },
+})
