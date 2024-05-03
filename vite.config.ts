97894773f0951efe0e5c7e9de6f1193f74b03835
BlackSoulHub <mr.asaglaev@mail.ru>
2024-05-03 04:16:57 +0000

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import dts from "vite-plugin-dts"
-import path from "path"
+import path, { extname, relative } from "path"
+import { fileURLToPath } from "url"
+import { glob } from "glob"
 
 export default defineConfig({
     plugins: [
@@ -12,10 +14,19 @@ export default defineConfig({
     ],
     build: {
         lib: {
-            entry: path.resolve(__dirname, "./src/index.ts"),
-            name: "mate",
+            entry: path.resolve(__dirname, "src/index.ts"),
+            name: "MyLib",
             formats: ["umd"],
             fileName: (format) => `mate-ts.${format}.js`,
+        },
+        rollupOptions: {
+            external: ["react", "react-dom", "styled-components"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
         },
     },
 })
